7a05c61c5fe47b163990913bbc4857b59cae3abb
martinrodfl <martinrodf@gmail.com>
2022-09-09 17:37:00 -0300

    Agregado de endpoints para actualizar datos de usuarios:email-nombre-password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register', 'userUpdate']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     public function login(Request $request)
